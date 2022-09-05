b74f681d10ed0dca2c7ca696ed329a1d60982cb8
Tamara Wala <tamwala@gmail.com>
2022-09-05 21:14:35 +0200

    Kodilla 14.4

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,37 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RestController
+@RequestMapping("/v1/tasks")
+public class TaskController {
+
+    @GetMapping
+    public List<TaskDto> getTasks() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping(value = "{taskId}")
+    public TaskDto getTask(@PathVariable Long taskId) {
+        return new TaskDto(1L, "test title", "test_content");
+    }
+
+    @DeleteMapping
+    public void deleteTask(Long taskId) {
+    }
+
+    @PutMapping
+    public TaskDto updateTask(TaskDto taskDto) {
+        return new TaskDto(1L, "Edited test title", "Test content");
+    }
+
+    @PostMapping
+    public void createTask(TaskDto taskDto) {
+    }
+}
