da8b547f03e0cab0d2e9e7e2fc56da61d0f10364
Tamara Wala <tamwala@gmail.com>
2022-09-13 17:31:05 +0200

    Kodilla 15

---
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -0,0 +1,34 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.controller.TaskNotFoundException;
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DbService {
+
+    private final TaskRepository repository;
+
+    public List<Task> getAllTasks() {
+        return repository.findAll();
+    }
+
+    public Task getTasksById(final Long id) throws TaskNotFoundException {
+        return repository.findById(id).orElseThrow(TaskNotFoundException::new);
+    }
+
+    public Task saveTask(final Task task) {
+        return repository.save(task);
+    }
+
+    public void deleteTask(final Long id) {
+        repository.deleteById(id);
+    }
+}
+
+
