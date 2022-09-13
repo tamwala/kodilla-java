c6d454abeccc281db637f4e8bdaef6409ed2106a
Tamara Wala <tamwala@gmail.com>
2022-09-13 19:01:13 +0200

    Kodilla 15 additional class

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,54 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Component
+@RestController
+@RequestMapping("/v1/tasks")
+@RequiredArgsConstructor
+public class TaskController {
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
+
+    @GetMapping
+    public List<TaskDto> getTasks() {
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
+    }
+
+    @GetMapping(value = "/{taskId}")
+    public ResponseEntity<TaskDto> getTask(@PathVariable Long taskId) throws TaskNotFoundException {
+        return ResponseEntity.ok(taskMapper.mapToTaskDto(service.getTasksById(taskId)));
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createTask(@RequestBody TaskDto taskDto) {
+        Task task = taskMapper.mapToTask(taskDto);
+        service.saveTask(task);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping(value = "/{taskId}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long taskId){
+        service.deleteTask(taskId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<TaskDto> updateTask(@RequestBody TaskDto taskDto) {
+        Task task = taskMapper.mapToTask(taskDto);
+        Task savedTask = service.saveTask(task);
+        return ResponseEntity.ok(taskMapper.mapToTaskDto(savedTask));
+    }
+}
