3139c1ed2222a753a92fc80e0eab244fc49ee997
Tamara Wala <tamwala@gmail.com>
2022-10-11 14:50:17 +0200

    Kodilla 19.4

---
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -0,0 +1,41 @@
+package com.crud.tasks.scheduler;
+
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
+import com.crud.tasks.trello.config.AdminConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EmailScheduler {
+
+    private static final String SUBJECT = "Tasks: Once a day email";
+    private final SimpleEmailService simpleEmailService;
+    private final TaskRepository taskRepository;
+    private final AdminConfig adminConfig;
+    private EmailScheduler emailScheduler;
+
+    @Scheduled(fixedDelay = 10000)
+    public void sendInformationEmail() {
+        long size = taskRepository.count();
+        simpleEmailService.send(
+                new Mail(
+                        adminConfig.getAdminMail(),
+                        SUBJECT,
+                        "Currently in database you got: " + size +  emailScheduler.countTasks(size),
+                        null
+                )
+        );
+    }
+
+    public String countTasks (long size) {
+        if (size > 0) {
+            return "tasks";
+        } else {
+            return "task";
+        }
+    }
+}
